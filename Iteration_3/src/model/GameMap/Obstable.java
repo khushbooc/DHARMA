022be8f7b3b66c69383a7c3b562991afc68d11cd
Giang Nguyen <gianghnguyen@ufl.edu>
2015-04-16 22:05:14 -0400

    adding Item, Obstable, Interactive

---
+++ Iteration_3/src/model/GameMap/Obstable.java
@@ -0,0 +1,22 @@
+package model.gameMap;
+
+/**
+ * Created by giangnguyen on 4/16/15.
+ */
+public class Obstable extends Item {
+
+    public Obstable(Location location, String name){
+        super(location, name);
+    }
+
+    @Override
+    public boolean isCollidable(){
+        return false;
+    }
+
+    @Override
+    public void onTouch(Entity e){
+        //TODO
+    }
+
+}
--- Iteration_3/src/model/GameMap/Tile.java
+++ Iteration_3/src/model/GameMap/Tile.java
@@ -3,23 +3,47 @@ package model.gameMap;
 
 
 public class Tile extends MapObject{
-	// private AreaEffect areaEffect;
+	private AreaEffect areaEffect;
+    private Terrain terrain;
+    private Item item;
+    private Entity entity;
 
-	public Tile(Location location){ //will take more parameters(maybe)
+    public Tile(Location location){
         super(location);
-	}
-	
-//	public Tile (Tile tile) {
-//		this.location = tile.getLocation();
-//	}
-
-	
-	public Location getLocation(){  // can be deleted later. Used for debugging.
-		return location;
+    }
+	public Tile(Location location, AreaEffect aoe, Terrain terrain, Item item, Entity entity){
+        super(location);
+        this.areaEffect = aoe;
+        this.terrain = terrain;
+        this.item = item;
+        this.entity = entity;
 	}
 
-	
-	
+    public AreaEffect getAreaEffect(){
+        return this.areaEffect;
+    }
+    public Terrain getTerrain(){
+        return this.terrain;
+    }
+    public Item getItem(){
+        return this.item;
+    }
+    public Entity getEntity(){
+        return this.entity;
+    }
+    public void setAreaEffect(AreaEffect aoe){
+        this.areaEffect = aoe;
+    }
+    public void setTerrain(Terrain terrain){
+        this.terrain = terrain;
+    }
+    public void setItem(Item item){
+        this.item = item;
+    }
+    public void setEntity(Entity entity){
+        this.entity = entity;
+    }
+
 	public String toString() {
 		String result = "";
 		result += "Tile:" + this.location.getX() + "," + this.location.getY();
