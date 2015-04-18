0f7b38751a4d2b0da56833c3d3cade9795e89055
ashishag <ashishjuit@yahoo.co.in>
2015-04-18 17:17:39 -0400

    Merge origin/master
    Conflicts:
    	FBG2.0/src/model/entity/ability/BasicAttack.java

--- Iteration_3/src/model/entity/NPC.java
+++ Iteration_3/src/model/entity/NPC.java
@@ -1,9 +1,28 @@
-/*import model.entity;
+import model.entity;
 
 public class NPC {
 	public NPC(Occupation occupation, Inventory inventory,
 			EquipmentContainer equicontainer) {
 		super(occupation, inventory, equicontainer);
 		
-
-}*/
+		protected Inventory createInventory(){
+			//TODO;
+			
+		protected boolean createFriendly(){
+			return false;
+		}
+		protected Inventory getInventory(){
+			//TODO;
+		}
+		
+		public Takeable sellItem(int position, Entity purchaser){
+			
+		}
+                //TODO;		
+		}
+ 
+		public void buyItem(Takeable item, Entity puchaser){
+			//TODO;
+		}
+		
+}
