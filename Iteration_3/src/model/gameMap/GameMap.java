e8378de318162bfa4aafbe43c024aff0b4bf485d
khushbooc <khushboochawla@ufl.edu>
2015-04-18 05:02:23 -0400

    mapmovement

--- Iteration_3/src/model/gameMap/GameMap.java
+++ Iteration_3/src/model/gameMap/GameMap.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +16,8 @@ public class GameMap
 	
 	private DrawingPanel panel;
 	private JFrame frame; 
+	private int lowx=3;
+	private int lowy=4;
 	//private Terrain[][] terrain;
 	
   public GameMap() {
@@ -43,7 +47,7 @@ public class GameMap
 	final static int BORDERS = 10;  
 	//final static int SCRSIZE = HEXSIZE * (BSIZE + 1) + BORDERS*3; //screen size (vertical dimension).
 	final static int WORLD_SIZE_X=1600;
-	final static int WORLD_SIZE_Y=1200;
+	final static int WORLD_SIZE_Y=700;
 	final static int VIEWPORT_SIZE_X=800;
 	final static int VIEWPORT_SIZE_Y=600;
 	//int[][] board = new int[BSIZE][BSIZE];
@@ -66,12 +70,23 @@ public class GameMap
  
 		GameMech.setHeight(HEXSIZE); //Either setHeight or setSize must be run to initialize the hex
 		GameMech.setBorders(BORDERS);
+		
+		frame = new JFrame("Hex Testing 4");
+		
+		//set up board here
+		//board[3][3] =(int)'A' ;
+		//board[4][3] = (int)'Q';
+		//board[4][4] = -(int)'B';
+	}
  
+	public void createAndShowGUI()
+	{
+		
 		for (int i=0;i<BSIZE;i++) {
 			for (int j=0;j<BSIZE;j++) {
 				//board[i][j]=EMPTY;
 				Location loc=new Location(i,j);
-				if((i>1 && i<BSIZE-2) && (j>1&&j<BSIZE-2)){
+				if((i>=lowx && i<=BSIZE-lowx) && (j>=lowy&&j<=BSIZE-lowy)){
 						
 						//terrain[i][j] = new Grass("Grass",grass);
 						map[i][j]=new Tile(loc, new Grass("Grass",grass));
@@ -83,28 +98,34 @@ public class GameMap
 			}
 		}
 		
-		//set up board here
-		//board[3][3] =(int)'A' ;
-		//board[4][3] = (int)'Q';
-		//board[4][4] = -(int)'B';
-	}
- 
-	private void createAndShowGUI()
-	{
 		panel = new DrawingPanel();
  
  
 		//JFrame.setDefaultLookAndFeelDecorated(true);
-		frame = new JFrame("Hex Testing 4");
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		
+		
+		//frame.getContentPane().setLayout(null);
 		Container content = frame.getContentPane();
 		content.add(panel);
-		panel.setLocation(100,100);
 		
+		//panel.setLocation(100,100);
+		panel.setPreferredSize(new Dimension(800,600));
+		//panel.setLayout(null);
+				
+		// setup our canvas size and put it into the content of the frame
+		//frame.setBounds(0,0,800,600);
+		//panel.add(this);
+		frame.pack();
 		frame.setSize( VIEWPORT_SIZE_X, VIEWPORT_SIZE_Y);
 		frame.setResizable(false);
-		frame.setLocationRelativeTo( null );
+		//frame.setLocationRelativeTo( null );
 		frame.setVisible(true);
+		
+		frame.setFocusable(true);
+        frame.setLayout(new FlowLayout());
+        //frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.repaint();
 	}
  
  
@@ -121,20 +142,20 @@ public class GameMap
 		public void paintComponent(Graphics g)
 		{
             Graphics2D g2 = (Graphics2D)g;
-			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+			//g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			//g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
 			super.paintComponent(g2);
 			//draw grid
 
 			for (int i=0;i<BSIZE;i++) {
 				for (int j=0;j<BSIZE;j++) {
-					GameMech.drawHex(i,j,g2);
+					GameMech.drawHex(i,j,map[i][j],g2);
 				}
 			}
 			
 			for (int i=0;i<BSIZE;i++) {
 				for (int j=0;j<BSIZE;j++) {					
-					GameMech.fillHex(i,j,map[i][j],g2);
+					//GameMech.fillHex(i,j,map[i][j],g2);
 				}
 			}
  
@@ -146,5 +167,14 @@ public class GameMap
 	public JFrame getFrame()
 	{
 		return this.frame;
+	}
+	public void setlocation(){
+		System.out.println("Location:"+lowx+","+lowy);
+		lowx=lowx-1;
+		lowy=lowy-1;
+		if(lowx<0)
+			lowx=0;
+		if(lowy<0)
+			lowy=0;
 	}
 }
