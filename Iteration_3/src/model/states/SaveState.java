487dd0bc1dc8144634bc804cb166c6c36f78a7c7
MW4 <mwestacott1@ufl.edu>
2015-04-20 06:53:25 -0400

    LoadState and SaveState added

---
+++ Iteration_3/src/model/states/SaveState.java
@@ -0,0 +1,17 @@
+package model.states;
+
+import model.Game;
+
+/**
+ * Created by agemery on 4/19/15.
+ */
+public class SaveState extends GameState{
+
+    public SaveState (Game game) {
+        super(game);
+    }
+
+    public void update() {
+        //TODO
+    }
+}
